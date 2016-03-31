5277f72e739e93b22e16a6b06c79d679511a290f
Sean <sdillon255@gmail.com>
2016-03-31 09:06:01 -0400

    add yahoo api library

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,31 @@
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
 /**
  * Created by Sean on 3/31/2016.
  */
 public class Hello {
+
+
+    public static void main(String args[]) {
+
+        try {
+            Stock stock = YahooFinance.get("INTC");
+
+            BigDecimal price = stock.getQuote().getPrice();
+            BigDecimal change = stock.getQuote().getChangeInPercent();
+            BigDecimal peg = stock.getStats().getPeg();
+            BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+
+            stock.print();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
