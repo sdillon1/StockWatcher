500ce94f7efa6307ab85fcfa0a6f7e4baec0bfa2
Sean <sdillon255@gmail.com>
2016-03-31 09:21:03 -0400

    ignore stuff

--- src/Hello.java
+++ src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
     public static void main(String args[]) {
 
         try {
-            Stock stock = YahooFinance.get("INTC");
+            Stock stock = YahooFinance.get("PSUN");
 
             BigDecimal price = stock.getQuote().getPrice();
             BigDecimal change = stock.getQuote().getChangeInPercent();
@@ -21,6 +21,10 @@ public class Hello {
             BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
 
             stock.print();
+
+
+            System.out.println("The current price of " + stock.getName() + " is: " + price);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
